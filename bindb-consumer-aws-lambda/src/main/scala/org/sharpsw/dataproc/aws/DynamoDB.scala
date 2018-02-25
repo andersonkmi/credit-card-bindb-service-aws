6528568c614b482b12d1a33b22f9b3006f1864cf
Anderson Ito <andersonkmi@acm.org>
2018-02-25 13:13:29 -0300

    Implementation for DynamoDB insert

--- bindb-consumer-aws-lambda/src/main/scala/org/sharpsw/dataproc/aws/DynamoDB.scala
+++ bindb-consumer-aws-lambda/src/main/scala/org/sharpsw/dataproc/aws/DynamoDB.scala
@@ -1,12 +1,42 @@
 package org.sharpsw.dataproc.aws
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
+import org.sharpsw.dataproc.AWSLambdaEnvVars.{BinDbDynamoTable, BinDbDynamoTableDefault}
 import org.sharpsw.dataproc.data.BinRecord
+
+import scala.collection.JavaConverters._
+import scala.util.Properties
 
 object DynamoDB {
   private val dynamoService = AmazonDynamoDBClientBuilder.standard.build
 
+  private val BinField: String = "bin"
+  private val BrandField: String = "brand"
+  private val BankField: String = "bank"
+  private val CardTypeField: String = "cardType"
+  private val LevelField: String = "level"
+  private val IsoCountryField: String = "isoCountry"
+  private val InfoField: String = "info"
+
   def putRecord(record: BinRecord): Boolean = {
-    true
+    val values = Map(
+      BinField -> new AttributeValue().withS(record.bin),
+      BrandField -> new AttributeValue().withS(record.brand),
+      BankField -> new AttributeValue().withS(record.bank),
+      CardTypeField -> new AttributeValue().withS(record.cardType),
+      LevelField -> new AttributeValue().withS(record.level),
+      IsoCountryField -> new AttributeValue().withS(record.isoCountry),
+      InfoField -> new AttributeValue().withS(record.info)
+    ).asJava
+
+    try {
+      dynamoService.putItem(Properties.envOrElse(BinDbDynamoTable, BinDbDynamoTableDefault), values)
+      true
+    } catch {
+      case _: Exception => {
+        false
+      }
+    }
   }
 }
