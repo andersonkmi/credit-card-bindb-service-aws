1449f850b1425fbb179e44bce23a3b80f1094ba7
Anderson Ito <andersonkmi@acm.org>
2018-03-03 08:36:37 -0300

    Mods

--- csv-reader-aws-lambda/src/main/scala/org/sharpsw/dataproc/Main.scala
+++ csv-reader-aws-lambda/src/main/scala/org/sharpsw/dataproc/Main.scala
@@ -1,14 +1,20 @@
 package org.sharpsw.dataproc
 
+import com.amazonaws.services.lambda.runtime.Context
+
 import scala.collection.JavaConverters._
 import com.amazonaws.services.lambda.runtime.events.S3Event
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord
 import org.sharpsw.dataproc.utils.S3FileReader.readFile
+import org.slf4j.LoggerFactory
 
 class Main {
+  private val logger = LoggerFactory.getLogger(getClass)
 
-  def processFileContents(event: S3Event): String = {
+  def processFileContents(event: S3Event, context: Context): String = {
     event.getRecords.asScala.foreach(processFile)
+    logger.info(s"Memory limit (MB): ${context.getMemoryLimitInMB}")
+    logger.info(s"Remaining time (ms): ${context.getRemainingTimeInMillis}")
     "OK"
   }
 
