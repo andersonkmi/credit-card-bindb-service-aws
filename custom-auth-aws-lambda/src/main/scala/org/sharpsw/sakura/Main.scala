6d971e120086eaf47579e1f6edeb997cf20bebc2
Anderson Ito <andersonkmi@acm.org>
2018-03-06 08:21:05 -0300

    Add a piece of code

---
+++ custom-auth-aws-lambda/src/main/scala/org/sharpsw/sakura/Main.scala
@@ -0,0 +1,9 @@
+package org.sharpsw.sakura
+
+import com.amazonaws.services.lambda.runtime.Context
+
+object Main {
+  def authenticate(event: Map[String, Object], context: Context): Unit = {
+
+  }
+}
