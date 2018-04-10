5698b3ff22ec68ee9c1629e0ae0d875db6a60c77
Anderson Ito <andersonkmi@acm.org>
2018-04-10 20:25:09 +0100

    dynamodb implementation

--- api-gw-aws-lambda/src/main/scala/org/sharpsw/sakura/service/AWSLambdaEnvVars.scala
+++ api-gw-aws-lambda/src/main/scala/org/sharpsw/sakura/service/AWSLambdaEnvVars.scala
@@ -3,4 +3,8 @@ package org.sharpsw.sakura.service
 object AWSLambdaEnvVars {
   val MethodArnHeader = "METHOD_ARN"
   val MethodArnHeaderDefault = "methodArn"
+
+  val BinDbDynamoTable = "BINDB_RECORD_TABLE"
+  val BinDbDynamoTableDefault = "BinDBRecord"
+
 }
