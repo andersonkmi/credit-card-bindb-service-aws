11dcdb56ebca3fddc8d1f1cf8e4bde627f413141
Anderson Ito <andersonkmi@acm.org>
2018-04-12 23:40:48 -0300

    Minor modifications

--- api-gw-aws-lambda/src/main/scala/org/sharpsw/sakura/Main.scala
+++ api-gw-aws-lambda/src/main/scala/org/sharpsw/sakura/Main.scala
@@ -8,11 +8,7 @@ import scala.collection.JavaConverters._
 class Main extends RequestHandler[java.util.Map[String, Object], BinDBResponse] {
 
   override def handleRequest(event: java.util.Map[String, Object], context: Context): BinDBResponse = {
-    println("Starting authentication process")
-
     val binNumber = if (event.asScala.exists(_._1 == "bin")) event.asScala("bin") else ""
-    println(binNumber)
-
     findRecord(binNumber.toString)
   }
 }
--- api-gw-aws-lambda/src/main/scala/org/sharpsw/sakura/service/AWSLambdaEnvVars.scala
+++ api-gw-aws-lambda/src/main/scala/org/sharpsw/sakura/service/AWSLambdaEnvVars.scala
@@ -1,9 +1,6 @@
 package org.sharpsw.sakura.service
 
 object AWSLambdaEnvVars {
-  val MethodArnHeader = "METHOD_ARN"
-  val MethodArnHeaderDefault = "methodArn"
-
   val BinDbDynamoTable = "BINDB_RECORD_TABLE"
   val BinDbDynamoTableDefault = "BinDBRecord"
 
